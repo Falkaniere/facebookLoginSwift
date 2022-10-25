9061f361f2a60166fa41038dda79991ca7617a99
Jonatas Falkaniere <jonatasfalkaniere@gmail.com>
2022-10-24 21:38:29 -0300

    chore: first commit

---
+++ facebookLogin/facebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookLoginApp.swift
+//  facebookLogin
+//
+//  Created by Jonatas Falkaniere on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct facebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
